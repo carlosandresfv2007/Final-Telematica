6878f1f1a6e9060f8482d16e0c08aef179b45705
carlosandresfv2007 <carlosandresflorezvargas2007@gmail.com>
2025-05-15 23:09:19 -0500

    prueba antes de examen 2

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Bienvenido al curso de Telemática PRUEBA CI/CD, prueba antes de la entrega'
+    return 'Bienvenido al curso de Telemática PRUEBA CI/CD, prueba antes de la entrega 2'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8090)
